e29a08bcf2478009e78a632a50af7a0c77a5f238
as16x <93331983+as16x@users.noreply.github.com>
2022-03-03 22:28:55 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = () => {
 
         <Navigation id='Navigation' className={change ? 'navbar colorChange' : 'none'}>
             
-            <h1><a href="#Hero">Cedrick</a></h1>
+            <h1><a href="#Hero">The Code Doctor</a></h1>
 
             <ul>
                 <li data-aos="fade-down"
