118cc6b02c2ded9fa7ff6afbc9e28cecc540fe28
as16x <93331983+as16x@users.noreply.github.com>
2022-03-15 21:46:52 +0100

    Update Hambourger.js

--- src/components/Hambourger.js
+++ src/components/Hambourger.js
@@ -6,54 +6,55 @@ import resume from '../Documents/resume-cedrick-lupembe.pdf'
 
 const Hambourger = () => {
 
-    const [showMenu, setShowMenu] = useState(false)
-    const [open, setOpen] = useState(false);
+  const [showMenu, setShowMenu] = useState(false)
+  const [open, setOpen] = useState(false);
 
 
-    const Allclose = () => {
-        setShowMenu(false)
-        setOpen(false)
-    }
+  const Allclose = () => {
+    setShowMenu(false)
+    setOpen(false)
+  }
 
-    let Menu
-    let MenuMask
-    let Burger
+  let Menu
+  let MenuMask
+  let Burger
 
-    if(showMenu){
+  if (showMenu) {
 
-        // Menu mobile
-        Menu = <MenuMobile onClick={() => setOpen(false) }>
+    // Menu mobile
+    Menu = <MenuMobile onClick={() => setOpen(false)}>
 
-                    <List>
-                      <a href="#Hero">Home</a>
-                      <a href="#About">About</a>
-                      <a href="#Works">Works</a>
-                      <a href="#Contact">Contact</a>
-                    </List>
+      <List>
+        <a href="#Hero">Home</a>
+        <a href="#About">Über mich</a>
+        <a href="#Works">Arbeit</a>
+        <a href="#Contact">Kontakt</a>
+      </List>
 
-                    <span><a href={resume} target='_blank' rel='noreferrer' download='resume-cedrick-lupembe'>Resume</a></span>
+      <span><a href={resume} target='_blank' rel='noreferrer' download='resume-cedrick-lupembe'>Resume</a></span>
 
-              </MenuMobile>
+    </MenuMobile>
 
-        MenuMask = <Overlay onClick={Allclose}></Overlay>
-    }
+    MenuMask = <Overlay onClick={Allclose}></Overlay>
+  }
 
-    Burger = <BurgerMenu open={open} onClick={() => setOpen(!open)}>
-                <div />
-                <div />
-                <div />
-            </BurgerMenu>
+  Burger = <BurgerMenu open={open} onClick={() => setOpen(!open)}>
+    <div />
+    <div />
+    <div />
+  </BurgerMenu>
 
 
   return (
 
     <Span onClick={() => setShowMenu(!showMenu)}>
-        {Burger}
-        {Menu}
-        {MenuMask}
+      {Burger}
+      {Menu}
+      {MenuMask}
     </Span>
 
   )
 }
 
 export default Hambourger;
+
